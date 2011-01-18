71e487712275490864fa9afd9e434093f18dea67
Derek Reiff <derek@NuanceAir.local>
2011-01-17 21:40:28 -0800

    Update`

--- plus_server.py
+++ plus_server.py
@@ -1,7 +1,8 @@
 import socket
 import sys
 
-HOST = 'http://block115396-r76.blueboxgrid.com/'
+# HOST = 'http://block115396-r76.blueboxgrid.com/'
+HOST = ''
 PORT = 40008
 
 s = None
@@ -9,5 +10,18 @@ s = None
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 s.bind((HOST, PORT))
-
 s.listen(1)
+conn, addr = s.accept()
+
+def addedFunction(receivedString):
+	newString = receivedString.split(',')
+	return newString
+
+while 1:
+	data = conn.recv(1024)
+	if not data: break
+	conn.send(addedFunction)
+	
+# print 'Variables received: %s and %s' % 
+
+conn.close()
