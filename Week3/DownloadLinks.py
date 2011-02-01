aad939fda531d07283e94115ab55ef12a2786cf9
Derek Reiff <derekr@NuancePro.local>
2011-01-31 17:28:43 -0800

    Added prelimary file (to be finished at home) for the BeautifulSoup-URL-downloader-thing

---
+++ Week3/DownloadLinks.py
@@ -0,0 +1,29 @@
+from BeautifulSoup import BeautifulSoup
+import urllib2
+from pprint import pprint
+import html2text
+import re
+
+website = urllib2.urlopen("http://briandorsey.info/uwpython/Internet_Programming_in_Python.html")
+
+#What we need to do:
+# - Download Files from the Topics column in table
+# - Download HTML (& save) from the Readings column in table
+
+soup = BeautifulSoup(website)
+
+print soup.html.head.title.string # Should use this, somehow, to print out the HREF name (Between the <a></a> tags)
+
+print "There are " + str(len(soup('a'))) + " <a> tags"#So this tells us that we have 87 a tags
+
+researchstring = "^*.pdf"
+
+aTags = soup.findAll('a', href=re.compile("[^*].pdf"))
+
+print dir(aTags)
+
+for item in aTags:
+	print item.string #Goal is to print out just the stuff, now.
+
+# So that works. Just need to put an http://briandorsey.info/uwpython/ at the beginning of each href
+
